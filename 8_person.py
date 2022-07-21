62681cd1b8bc07251152f210defb675a7670742c
Erin <22713221+erin-weeks@users.noreply.github.com>
2022-07-21 19:47:08 -0400

    Update 8_person to add 'age'
    Included age as an optional parameter in function build_person.

--- 8_person.py
+++ 8_person.py
@@ -2,9 +2,13 @@
 
 '''This exercise takes various parts of a name and then returns a dicionary.'''
 
-def build_person(first_name, last_name):
+def build_person(first_name, last_name, age = ''):
 	'''Return a dictionary of information about a person'''
 	person = {'first': first_name, 'last': last_name}
+	
+	'''Because age is optional, we put it in an if statement just in case.'''
+	if age:
+		person['age'] = age
 	return person
 
 musician = build_person('jimi','hendrix')
@@ -13,3 +17,9 @@ print(musician)
 '''If we know the keys, can we format better?'''
 
 print(musician['first'].title() + " " + musician['last'].title())
+
+'''Testing with age'''
+
+musician = build_person('jimi', 'hendrix', 37)
+
+print(musician['first'].title() + " " + musician['last'] + ", " + str(age))
