1dd94832e2ae79e0b8342bbf51e6248de0c58bbe
Erin <22713221+erin-weeks@users.noreply.github.com>
2022-07-21 19:33:17 -0400

    Update 8_formatted_name.py
    Revised middle_name parameter to take an empty argument (made it optional).

--- 8_formatted_name.py
+++ 8_formatted_name.py
@@ -1,15 +1,27 @@
 #Working with return
 '''
-Round 2: Including an optional argument. 
+Round 3: Including an optional argument. 
 Add middle name
+But not everyone has a middle name.
+So we give it an empty default argument.
+Remember that parameters with defaults have to be at the end!
 '''
 
 #You must provide a variable where the returned information can be stored.
 
-def get_formatted_name(first_name, middle_name, last_name):
+def get_formatted_name(first_name, last_name, middle_name = ''):
 	'''Return a full name, neatly formatted.'''
-	full_name = first_name + " " + middle_name + " " + last_name
+	
+	'''Check whether there is a middle name.'''
+	if middle_name:
+		full_name = first_name + " " + middle_name + " " + last_name #include middle name
+	else:
+		full_name = first_name + " " + last_name #exclude middle name
+	
 	return full_name.title()
 
-musician = get_formatted_name('jimi', 'john', 'hendrix')
-print(musician)	
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
+musician = get_formatted_name('john','hooker','lee')
+print(musician)
