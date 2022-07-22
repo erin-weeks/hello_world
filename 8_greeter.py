3c9256c2e67c32bc03b25b526f4b4eab0e9f051c
Erin <22713221+erin-weeks@users.noreply.github.com>
2022-07-21 20:15:53 -0400

    Function in while loop
    Practice using a while loop in combination with a function (or two).

--- 8_greeter.py
+++ 8_greeter.py
@@ -1,11 +1,36 @@
 #This greeter program is to explore functions
 
-name = input("What's your name? ")
+'''Expanding on functions to review how to include functions in loops'''
 
-def greet_user(user_name):
-	"""Display a greeting"""
-	print("Hello, " + user_name.title())	
+people_in_line = True
 
-greet_user(name)
+def get_formatted_name(first_name, last_name):
+	"""Format full name"""
+	full_name = first_name + " " + last_name
+	
+	return full_name
 
+def greet_user(format_name):
+	"""Display a greeting"""	
+	print("\nHello, " + format_name.title())	
+	
 greet_user('Emily')
+
+
+while people_in_line == True:
+	print("\nPlease tell me your name.") #get name
+	f_name = input("First name: ")
+	l_name = input("Last name: ")
+	
+	formatted_name = get_formatted_name(f_name, l_name) #call format function
+	greet_user(formatted_name) #call greet function
+	
+	'''Prevent infinite loops by including a flag.'''
+	more_people = input("Are there more people to greet? (yes / no ) ")
+	if more_people == 'no':
+		people_in_line = False
+
+	
+	
+	
+	
